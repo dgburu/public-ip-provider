9f1fcc23687d5a8c36cb68812c8a8e919fcfe603
dgburu <dgburu@gmail.com>
2016-06-17 00:09:03 +0200

    added comment about users file format

--- publicipprovider/src/com/dgbsoft/pip/provider/user/UserCheck.java
+++ publicipprovider/src/com/dgbsoft/pip/provider/user/UserCheck.java
@@ -12,26 +12,33 @@ import javax.servlet.http.HttpServletRequest;
 import com.dgbsoft.pip.provider.data.BlockedIp;
 import com.dgbsoft.pip.provider.data.DataStoreFactoryService;
 
+/**
+ * check /WEB-INF/users.properties file that should contain the following info.
+ * usernname=usertype username.pwd=password
+ * 
+ * @author alkahuete
+ *
+ */
 public class UserCheck {
 
 	public static Logger logger = Logger.getLogger(UserCheck.class.getName());
 
 	private static UserCheck INSTANCE = new UserCheck();
-	
+
 	public static UserCheck getInstance() {
 		return INSTANCE;
 	}
-	
+
 	public boolean checkUser(HttpServletRequest req, ServletContext context) {
 		EntityManager em = DataStoreFactoryService.get().createEntityManager();
 
 		String user = req.getParameter("usr");
-        String remoteAddress = req.getRemoteAddr();
+		String remoteAddress = req.getRemoteAddr();
 
-        if (user == null) {
+		if (user == null) {
 			try {
 				BlockedIp blockedIp = em.find(BlockedIp.class, remoteAddress);
-				if (blockedIp == null) {	
+				if (blockedIp == null) {
 					blockedIp = new BlockedIp(remoteAddress);
 					em.persist(blockedIp);
 				}
@@ -41,11 +48,11 @@ public class UserCheck {
 				logger.warning(e.toString());
 			}
 
-            return false;
-        } else {
+			return false;
+		} else {
 			try {
 				BlockedIp blockedIp = em.find(BlockedIp.class, remoteAddress);
-				if (blockedIp != null) {	
+				if (blockedIp != null) {
 					logger.info("nok, bocked ip " + remoteAddress);
 					return false;
 				}
@@ -56,50 +63,50 @@ public class UserCheck {
 			}
 
 			InputStream usersStream = context.getResourceAsStream("/WEB-INF/users.properties");
-        	Properties allowedUsers = new Properties();
-        	try {
-        		allowedUsers.load(usersStream);
-        	} catch (IOException e) {
-        		logger.severe("error loading users" + e.getMessage());
-        		return false;
-        	}
-        	if (!allowedUsers.containsKey(user)) {
-    			try {
-    				BlockedIp blockedIp = em.find(BlockedIp.class, remoteAddress);
-    				if (blockedIp == null) {	
-    					blockedIp = new BlockedIp(remoteAddress);
-    					em.persist(blockedIp);
-    				}
-        			logger.warning("invalid user = " + user + ", rip = " + remoteAddress);
-    			} catch (Exception e) {
-    				logger.info("nok cannot check ip");
-    				logger.warning(e.toString());
-    			}
+			Properties allowedUsers = new Properties();
+			try {
+				allowedUsers.load(usersStream);
+			} catch (IOException e) {
+				logger.severe("error loading users" + e.getMessage());
+				return false;
+			}
+			if (!allowedUsers.containsKey(user)) {
+				try {
+					BlockedIp blockedIp = em.find(BlockedIp.class, remoteAddress);
+					if (blockedIp == null) {
+						blockedIp = new BlockedIp(remoteAddress);
+						em.persist(blockedIp);
+					}
+					logger.warning("invalid user = " + user + ", rip = " + remoteAddress);
+				} catch (Exception e) {
+					logger.info("nok cannot check ip");
+					logger.warning(e.toString());
+				}
 
-    			return false;
-        	} else {
-        		logger.info("logged user = " + user);
-        		String type = allowedUsers.getProperty(user);
-        		req.getSession().setAttribute("login", type);
-        		String pass = req.getParameter("password");
-        		if ("ipsetter".equals(type)) {
-            		logger.info("user " + user + "is ipsetter type");
-            		return true;
-        		}
-            	if (!allowedUsers.containsKey(user + ".pwd")) {
-    				logger.severe("no passwd set for this user");
-            		return false;
-            	} else {
-            		if (allowedUsers.getProperty(user + ".pwd", "").equals(pass)) {
-                		req.getSession().setAttribute("password", pass);
-                		return true;
-            		} else {
-        				logger.severe("passwd does not match for user " + user);
-                		return false;
-            		}
-            	}
-        	}
-        }
+				return false;
+			} else {
+				logger.info("logged user = " + user);
+				String type = allowedUsers.getProperty(user);
+				req.getSession().setAttribute("login", type);
+				String pass = req.getParameter("password");
+				if ("ipsetter".equals(type)) {
+					logger.info("user " + user + "is ipsetter type");
+					return true;
+				}
+				if (!allowedUsers.containsKey(user + ".pwd")) {
+					logger.severe("no passwd set for this user");
+					return false;
+				} else {
+					if (allowedUsers.getProperty(user + ".pwd", "").equals(pass)) {
+						req.getSession().setAttribute("password", pass);
+						return true;
+					} else {
+						logger.severe("passwd does not match for user " + user);
+						return false;
+					}
+				}
+			}
+		}
 	}
-	
+
 }
